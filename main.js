d92431b33f345a7518ab6e71dcec9daad3c6e3e4
petertorelli <peter@petertorelli.com>
2020-01-29 14:53:20 -0800

    Updated to latest version of serial.

--- main.js
+++ main.js
@@ -2,17 +2,13 @@
 
 'use strict';
 
-const serialport = require('serialport');
-const parser = new serialport.parsers.Readline;
-const sprintf = require('sprintf');
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: '',
-});
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const sprintf = require('sprintf')
+const readline = require('readline')
 
-let port; // forgot how to pass data through promise chains
+let PORT
+let PARSER
 
 const argv = require('yargs')
 	.option('c', {
@@ -29,15 +25,9 @@ const argv = require('yargs')
 		nargs: 1,
 	})
 	.option('p', {
-		alias: 'pid',
+		alias: 'path',
 		type: 'string',
-		describe: 'Connect to the first product ID found that matches',
-		nargs: 1,
-	})
-	.option('n', {
-		alias: 'name',
-		type: 'string',
-		describe: 'Connect to actual port name',
+		describe: 'Connect to actual port device path',
 		nargs: 1,
 	})
 	.option('l', {
@@ -56,130 +46,86 @@ const argv = require('yargs')
 	.exitProcess(true)
 	.help('h')
 	.alias('h', 'help')
-	.argv;
+	.argv
 
-
-function processLine(port) {
-	rl.on('line', (line) => {
-		if (line === 'exit' || line === 'q') {
-			port.close();
-			process.exit(0);
-		} else {
-			// this is a workaround for the weird-ass macOS+L476+8B bug.
-			line = '' + line;
-			for (let i = 0; i < line.length; ++i) {
-				port.write(line[i]);
-			}
-			if (argv.crlf) { 
-				port.write('\r\n');
-			}
-		}
-	});
-}
 
 if (argv.list) {
-	getPortsP().then(ports => {
+	SerialPort.list()
+	.then(ports => {
 		console.log(sprintf('%-30s %10s %10s %s', 
 			'manufacturer',
 			'vendorId',
 			'productId',
-			'comName'
-		));
+			'path'
+		))
 		ports.forEach(portObj => {
 			if (portObj.manufacturer) {
 				console.log(sprintf('%-30s %10s %10s %s', 
 					portObj.manufacturer,
 					portObj.vendorId,
 					portObj.productId,
-					portObj.comName
-				));
+					portObj.path
+				))
 			} else {
 				console.log(sprintf('%-30s %10s %10s %s', 
-					'?', '?', '?', portObj.comName
-				));
+					'?', '?', '?', portObj.path
+				))
 			}
-		});
+		})
+	})
+	.catch(err => {
+		console.error(err)
+	})
+} else if (argv.path) {
+	console.log('Opening port.')
+	PORT = new SerialPort(argv.path, {
+		baudRate: argv.baud,
+	}, err => {
+		if (err) {
+			console.error('SerialPort Open Error:', err)
+		} else {
+			console.log('Port is open.')
+		}
+	})
+	PORT.on('error', err => {
+		console.error('SerialPort Error:', err)
+	})
+	PARSER = PORT.pipe(new Readline({ delimiter: '\r\n' }))
+	PARSER.on('data', data => {
+		console.log(data)
+	})
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '',
+})
+.on('line', (line) => {
+	if (line === 'exit' || line === 'q') {
+		PORT.close();
 		process.exit(0);
-	}).catch(err => {
-		console.error('Error: ' + err);
-	});
-} else if (argv.pid && argv.name) {
-	console.error('--pid and --name are mutually exclusive');
-	process.exit(1);
-} else if (argv.pid) {
-	getPortsP()
-	.then(ports => connectPortP(ports, argv.pid, argv.baud))
-	.then(port => { processLine(port); })
-	.catch(err => { 
-		console.error('Error: ' + err);
-		process.exit(1);
-	});
-} else if (argv.name) {
-	getPortsP()
-	.then(ports => connectNameP(ports, argv.name, argv.baud))
-	.then(port => { processLine(port); })
-	.catch(err => { 
-		console.error('Error: ' + err);
-		process.exit(1);
-	});
-}
-
-function getPortsP() {
-	return new Promise((resolve, reject) => {
-		serialport.list((err, ports) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(ports);
-			}
-		});
-	});
-}
-
-function connectNameP(ports, name, baud) {
-	console.log(baud)
-	return new Promise((resolve, reject) => {
-		let found = undefined;
-		ports.forEach(port => {
-			if (port.comName == name) {
-				found = port.comName;
-			}
-		});
-		if (found) {
-			port = new serialport(found, { baudRate: baud, });
-			port.pipe(parser);
-			parser.on('open', () => {
-				console.log(`[${found}]: open`);
+	} else {
+		// TODO: Need a Tx delay per char for some devices (e.g., STLink)
+		line = '' + line;
+		for (let i = 0; i < line.length; ++i) {
+			PORT.write(line[i], err => {
+				if (err) {
+					console.error(err)
+				}
 			});
-			resolve(port);
-		} else {
-			reject('Unable to locate name ' + name);
 		}
-	});
-}
-
-function connectPortP(ports, pid, baud) {
-	console.log(baud)
-	return new Promise((resolve, reject) => {
-		let found = undefined;
-		ports.forEach(port => {
-			if (port.productId == pid) {
-				found = port.comName;
-			}
-		});
-		if (found) {
-			port = new serialport(found, { baudRate: baud, });
-			port.pipe(parser);
-			parser.on('data', data => {
-				console.log(`[${found}]: data: ${data}`);
-			});
-			resolve(port);
-		} else {
-			reject('Unable to locate PID ' + pid);
+		if (argv.crlf) { 
+			PORT.write('\r\n');
 		}
-	});
-}
-
-
-
+	}
+})
+.on('SIGINT', () => {
+	console.log('CTRL-C')
+	if (PORT.isOpen) {
+		console.log('Closing port.')
+		PORT.close()
+		process.exit()
+	}
+})
 
